401f38c398b17aca25262d46ac119bb09ef46ca2
Javier Moreno <javier.moreno@correo.ucu.edu.uy>
2024-03-23 22:34:05 -0300

    Main arreglado para jugar varias veces

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import montyhall
 
-vecesJugadas = 1000
-vecesGanadasCambiando = 0
-vecesGanadasSinCambiar = 0
+def jugarVariasVeces(vecesJugadas):
+    vecesGanadasCambiando = 0
+    vecesGanadasSinCambiar = 0
 
-for i in range(vecesJugadas):
-    if montyhall.montyHall(True):
-        vecesGanadasCambiando += 1
-    if montyhall.montyHall(False):
-        vecesGanadasSinCambiar += 1
+    for i in range(vecesJugadas):
+        if montyhall.montyHall(True):
+            vecesGanadasCambiando += 1
+        if montyhall.montyHall(False):
+            vecesGanadasSinCambiar += 1
 
-print(f"Al jugar {vecesJugadas} veces, se ganaron {vecesGanadasCambiando} veces al cambiar y {vecesGanadasSinCambiar} veces al no cambiar")
+    print(f"Al jugar {vecesJugadas} veces, se ganaron {vecesGanadasCambiando} veces al cambiar y {vecesGanadasSinCambiar} veces al no cambiar")
     
+jugarVariasVeces(1000)
+jugarVariasVeces(10000)
+jugarVariasVeces(100000)
